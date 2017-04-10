930b3cf57833e1a0e867e0cdd04ed7c5f373e6f3
=RuggeroMegalinux <=grando.ruggero@gmail.com>
2017-04-10 13:10:21 +0200

    Aggiunta la struttura dati seguente: ([key],[url1,url2,url3]). Questa struttura mi permette di associare ad una keyword diverse urls. Per ora sono soltanto delle liste, da pensare quindi ad una DBMS in cui salvare le informazioni trovate.

--- main.py
+++ main.py
@@ -36,11 +36,44 @@ def unione(p,q):
         if e not in p:
             p.append(e)
 
+
+# pagina, l'ho definita url....nel secondo campo di input della procedura seguente
+# Nella seguente procedura il contenuto trovato viene suddiviso in parole. Per ogni parola
+# si chiama la procedura aggiungi_indice che aggiunge o meno la parola e l'url all'indice
+def aggiungi_pagina_indice (indice,url,contenuto):
+       parole = contenuto.split()
+       for parola in parole:
+                aggiungi_indice(indice,parola,url)
+
+#indice e' composto da un vettore che nel primo elemento(entry), 
+#contiene il valore keyword e nel secondo una lista con gli url ad esso connessi
+#La seguente procedura se trova una keyword gia' contenuta nell'indice aggiunge url, altrimenti sia la keywork che l'url
+def aggiungi_indice(indice,keyword,url):
+        for entry in indice:  
+            if entry[0] == keyword:
+                entry[1].append(url)
+
+        indice.append([keyword,[url]])
+
+
+
+#Nella seguente procedura si cerca nell'indice la keyword e si ritornano gli url ad essa associati.
+#se non vi sono non ritorna nulla
+def ricerca(indice,keyword):
+          for entry in indice:
+               if entry[0] == keyword:
+                   return entry[1]
+
+          return[]                                        
+
+
 #registro nella lista nonvisitati il seed (il sito origine). Nella lista visitati vi sono i
-#links gia visitati
+#links gia visitati.
+
 def crawler_engine_uno(seed):
     nonvisitati = [seed]
     visitati = []
+    indice = [] #aggiungi un indice dove eseguire le ricerce
     while nonvisitati:
         pagina = nonvisitati.pop()
         if pagina not in visitati:
@@ -48,7 +81,8 @@ def crawler_engine_uno(seed):
             print get_tutti_i_links(get_page(pagina))
             print "link non visitati prima di unione"
             print nonvisitati
-          
+            contenuto = get_page(pagina)
+            aggiungi_pagina_indice(indice,pagina,contenuto)
             unione(nonvisitati,get_tutti_i_links(get_page(pagina)))
             print "link non visitati dopo unione"
             print nonvisitati
@@ -61,9 +95,10 @@ def crawler_engine_uno(seed):
 
 
 
-    return visitati
+    return indice
 
 #Devo trovare un link che non sia troppo pieno di links!!!    
+#Nella seguente aggiungete il primo link (http....) o seme da cui partire
+print crawler_engine_uno('xxxxxxxxxxxxxxxxx')
 
-print crawler_engine_uno('http://www.megaoverclock.it/')
 
